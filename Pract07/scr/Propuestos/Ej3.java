71f876bfe6ad61f8739014597bfb1a8def83c33f
Basily-Castellanos-Ampuero <bcastellanos@unsa.edu.pe>
2025-07-01 22:20:42 -0500

    Update Ej3.java

--- Pract07/scr/Propuestos/Ej3.java
+++ Pract07/scr/Propuestos/Ej3.java
@@ -1,5 +1,14 @@
 package Propuestos;
 
 public class Ej3 {
-    
+     public static void main(String[] args) {
+        AVLTree<Integer> avl = new AVLTree<>();
+
+        int[] valores = {50, 30, 70, 20, 40, 60, 80};
+        for (int val : valores) {
+            avl.insert(val);
+        }
+
+        avl.visualizarAVL();
+    }
 }
